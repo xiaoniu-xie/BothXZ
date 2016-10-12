0196ca3d6e9a3e100a0bdb6ead14690111ced085
张伟 <1203008460@qq.com>
2016-10-12 17:04:20 +0800

    Initial Commit

---
+++ DangCargo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DangCargo
+//
+//  Created by 张伟 on 2016/10/12.
+//  Copyright © 2016年 DangerousCargo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DangCargo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DangCargo
+//
+//  Created by 张伟 on 2016/10/12.
+//  Copyright © 2016年 DangerousCargo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
